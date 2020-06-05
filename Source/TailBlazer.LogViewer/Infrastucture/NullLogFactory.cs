fd1103dd41d5b9aaeb0b0386d5200be5f0d7d4b5
Kus <michal.kus@starbeta.com.au>
2020-06-05 11:37:14 +0200

    Resolved compilation errors

---
+++ Source/TailBlazer.LogViewer/Infrastucture/NullLogFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TailBlazer.Domain.Infrastructure;
+
+namespace TailBlazer.Infrastucture
+{
+    public class NullLogFactory : ILogFactory
+    {
+        public ILogger Create(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            return new NullLogger();
+        }
+        public ILogger Create<T>()
+        {
+            return new NullLogger();
+        }
+    }
+}
--- Source/TailBlazer.LogViewer/ServiceLocator.cs
+++ Source/TailBlazer.LogViewer/ServiceLocator.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
 using StructureMap;
+using TailBlazer.Domain.FileHandling;
+using TailBlazer.Domain.FileHandling.Search;
+using TailBlazer.Domain.Formatting;
 using TailBlazer.Domain.Infrastructure;
+using TailBlazer.Domain.Settings;
 using TailBlazer.Infrastucture;
 using TailBlazer.Infrastucture.KeyboardNavigation;
 using TailBlazer.Views;
@@ -14,7 +18,7 @@ using TailBlazer.Views.Tail;
 
 namespace TailBlazer.LogViewer
 {
-    public static class ServiceLocator
+    public class ServiceLocator
     {
         private static Lazy<IObjectProvider> _defaultLazy = new Lazy<IObjectProvider>(ObjectProviderFactory);
         public static IObjectProvider Default => _defaultLazy.Value;
@@ -24,6 +28,20 @@ namespace TailBlazer.LogViewer
             var tempWindowToGetDispatcher = new Window();
             var container = new Container(x =>
             {
+                x.For<ILogger>().Use<NullLogger>();
+                x.For<ILogFactory>().Use<NullLogFactory>();
+
+                x.For<ISelectionMonitor>().Use<SelectionMonitor>();
+                x.For<ISearchInfoCollection>().Use<SearchInfoCollection>();
+                x.For<ISearchMetadataCollection>().Use<SearchMetadataCollection>().Transient();
+                x.For<ICombinedSearchMetadataCollection>().Use<CombinedSearchMetadataCollection>().Transient();
+                
+                x.For<ITextFormatter>().Use<TextFormatter>().Transient();
+                x.For<ILineMatches>().Use<LineMatches>();
+                x.For<ISettingsStore>().Use<FileSettingsStore>().Singleton();
+                x.For<IFileWatcher>().Use<FileWatcher>();
+
+
                 x.For<ObjectProvider>().Singleton();
                 x.Forward<ObjectProvider, IObjectProvider>();
                 x.Forward<ObjectProvider, IObjectRegister>();
@@ -32,10 +50,32 @@ namespace TailBlazer.LogViewer
                 x.For<TailViewModelFactory>().Use<TailViewModelFactory>().Singleton();
 
                 x.For<IKeyboardNavigationHandler>().Use<KeyboardNavigationHandler>();
+
+                x.Scan(scanner =>
+                {
+                    scanner.ExcludeType<ILogger>();
+
+                    //to do, need a auto-exclude these from AppConventions
+                    scanner.ExcludeType<SelectionMonitor>();
+                    scanner.ExcludeType<SearchInfoCollection>();
+                    scanner.ExcludeType<SearchMetadataCollection>();
+                    scanner.ExcludeType<CombinedSearchMetadataCollection>();
+                    scanner.ExcludeType<TextFormatter>();
+                    scanner.ExcludeType<LineMatches>();
+
+                    scanner.ExcludeType<FileWatcher>();
+                    scanner.LookForRegistries();
+                    scanner.Convention<AppConventions>();
+
+                    scanner.AssemblyContainingType<ILogFactory>();
+                    scanner.AssemblyContainingType<ServiceLocator>();
+                });
             });
             container.Configure(x => x.For<Dispatcher>().Add(tempWindowToGetDispatcher.Dispatcher));
+            container.GetInstance<StartupController>();
             tempWindowToGetDispatcher.Close();
             var provider = container.GetInstance<IObjectProvider>();
+
             return provider;
         }
     }
