281db8d329287bdd023f6ebfd886d6be7cafb6c0
Kus <michal.kus@starbeta.com.au>
2020-06-08 08:39:13 +0200

    Moved most of wpf specific code to seperate dll

---
+++ Source/TailBlazer.LogViewer.Wpf/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Windows;
+using System.Windows.Markup;
+
+
+[assembly: XmlnsPrefix("http://tailblazer.net/winfx/xaml/tailblazer", "tailblazer")]
+[assembly: XmlnsDefinition("http://tailblazer.net/winfx/xaml/tailblazer", "TailBlazer.LogViewer")]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page,
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page,
+    // app, or any theme specific resource dictionaries)
+)]
